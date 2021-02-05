bbdbb921df51de6d5142cb8329c9753355ed3bd9
Elian Aguilar <el.aguilar.ab@gmail.com>
2021-02-04 21:06:13 -0300

    Ejercicio del 1 al 10

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,10 +1,28 @@
 /*
+
+Aguilar Elian
+
+
 Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var numeroSueldo;
+	var numeroResultado;
+	var numeroAumento;
+	var numeroPorcentaje;
 
-	alert("ok");
+	numeroSueldo=document.getElementById('txtIdSueldo').value;
+	numeroSueldo=parseInt(numeroSueldo);
+	
+	numeroPorcentaje=prompt("Ingrese el porcentaje","Ingrese aqui")
+	numeroPorcentaje=parseInt(numeroPorcentaje);
+	numeroAumento=(numeroSueldo*numeroPorcentaje)/100
+	//numeroAumento=numeroSueldo*0.10;
+
+	numeroResultado=numeroSueldo+numeroAumento;
+	document.getElementById('txtIdResultado').value=numeroResultado;
+	
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,9 +1,30 @@
 /*
+
+Aguilar Elian
+
 Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	
+	var importe;
+	var descuento;
+	var resultado;
+	var porcentaje;
+
+	importe=document.getElementById('txtIdImporte').value;
+	importe=parseInt(importe);
+
+	porcentaje=prompt("Ingrese el porcentaje");
+	porcentaje=parseInt(porcentaje);
+
+	descuento=(importe*porcentaje)/100;
+	//descuento=(importe*25)/100;
+
+	resultado=importe-descuento;
+
+	document.getElementById('txtIdResultado').value=resultado;
+
 }
